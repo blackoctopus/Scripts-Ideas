0bccbd177af4cf054e2e29c11c46745fb4efcc7e
Philip Mozolak <philip.mozolak@icloud.com>
2014-03-17 17:04:05 -0400

    Added podcast shell

---
+++ podcast.sh
@@ -0,0 +1,6 @@
+
+
+osascript -e "tell application \"Piezo\"" -e "activate" -e "end tell"
+osascript -e "tell application \"Audio Hijack Pro\"" -e "activate" -e "end tell"
+osascript -e "tell application \"Skype\"" -e "activate" -e "end tell"
+osascript -e "tell application \"Terminal\"" -e "quit" -e "end tell"
